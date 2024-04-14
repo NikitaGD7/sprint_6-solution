dc7d2d6608e8b35fd4c26f6225f83e554581350b
Nikita GD <nikitagorbenko22@gmail.com>
2024-04-14 13:35:07 +0300

    ошибки исправлены

--- src/service/InMemoryTaskManager.java
+++ src/service/InMemoryTaskManager.java
@@ -167,6 +167,7 @@ public class InMemoryTaskManager implements TaskManager {
     @Override
     public void deleteTasks() {
         tasks.clear();
+        historyManager.removeAllTasks();
     }
 
     @Override
@@ -176,12 +177,14 @@ public class InMemoryTaskManager implements TaskManager {
             epic.clearSubtaskIds();
             updateEpicStatus(epic.getId());
         }
+        historyManager.removeAllEpics();
     }
 
     @Override
     public void deleteEpics() {
         epics.clear();
         subtasks.clear();
+        historyManager.removeAllSubtasks();
     }
 
     private int generateId() {
