7db381a826df44628b6081a88e21e16f2cef7663
Nikita GD <nikitagorbenko22@gmail.com>
2024-04-14 18:31:43 +0300

    errors were fixed

--- src/service/HistoryManager.java
+++ src/service/HistoryManager.java
@@ -11,9 +11,4 @@ public interface HistoryManager {
     List<Task> getHistory();
     void remove(int id);
 
-    void removeAllTasks();
-
-    void removeAllEpics();
-
-    void removeAllSubtasks();
 }
